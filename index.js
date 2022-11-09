7d270fbaf27ba701de1db04ea4323c48b0de599d
Baidyanath-K <baidyanathkumar12091998@gmail.com>
2022-11-10 01:04:43 +0600

    mongodb add

--- index.js
+++ index.js
@@ -3,10 +3,32 @@ const express=require('express');
 const app =express();
 const port=process.env.PORT||5000;
 const cors=require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 
 
 app.use(cors());
 app.use(express.json());
+
+const userName=process.env.DB_USER;
+const password=process.env.DB_PASSWORD;
+
+const uri = `mongodb+srv://${userName}:${password}@cluster0.hymefqt.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run(){
+    try{
+        const userALLCollection=client.db('Photography').collection('userCollention');
+        app.get('/services', async(req,res)=>{
+            const query={};
+            const cursor=userALLCollection.find(query);   
+            const services=await cursor.toArray();
+            res.send(services);
+        });
+    }
+    finally{}
+}
+run().catch(err=>console.log(err));
+
 
 app.get('/', (req,res)=>{
     res.send('Photography');
