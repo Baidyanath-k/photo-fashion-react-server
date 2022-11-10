5f40c803f48a9d73c044b0b05af92845b8be477e
Baidyanath-K <baidyanathkumar12091998@gmail.com>
2022-11-10 13:59:48 +0600

    specific id set up

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express=require('express');
 const app =express();
 const port=process.env.PORT||5000;
 const cors=require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 
@@ -18,12 +18,22 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
     try{
         const userALLCollection=client.db('Photography').collection('userCollention');
+        
         app.get('/services', async(req,res)=>{
             const query={};
             const cursor=userALLCollection.find(query);   
             const services=await cursor.toArray();
             res.send(services);
         });
+
+        app.get('/services/:id', async(req,res)=>{
+            const id=req.params.id;
+            const query={_id:ObjectId(id)}
+            const service=await userALLCollection.findOne(query);
+            res.send(service);
+        })
+        
+
     }
     finally{}
 }
